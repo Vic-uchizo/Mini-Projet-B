36aba580e354d657b56b7a05e85610fff36541b3
kille <killegavre@gmail.com>
2026-06-02 15:16:24 -0400

    fonction calculer_f créée calcule la valeur du polynome à x

--- simpson.py
+++ simpson.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-# Méthode Python : 
 
 
+# Méthode python
 
-
-
-
+def calculer_f(polynome, x):
+    f = polynome[0] + polynome[1]*x + polynome[2]*x*x + polynome[3]*x*x*x
+    return f
 
 
 
