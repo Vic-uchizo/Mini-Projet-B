30f870329deac9e9b2cd3cf3534d6c7a120ef65b
Vic-uchizo <vpradelm@gmail.com>
2026-06-02 16:54:50 -0400

    add methode moindre carre V2

--- moindre_rectangle.py
+++ moindre_rectangle.py
@@ -1,15 +1,12 @@
+from main import *
+import timeit
+import numpy as np
+
 ### Module de programmation de la methode des moindres carres###
 ## Fonction de type p1 + p2*inf+p3*inf^2 +p4*inf^3 ###
-##inter
-from main import polynome
-import timeit
 
 inf=40
 sup=50
-a=12
-b=13
-c=14
-d=15
 n=100
 
 
@@ -51,7 +48,23 @@ print("Methode numerique : ",calculer_moindre_rectangle_python(inf,sup,n))
 print("Erreur ", erreur_python_numerique(inf,sup,n,a,b,c,d))
 
 # Mesure du temps avec timeit
-temps = timeit.timeit(lambda: calculer_moindre_rectangle_python(inf,sup,n), number=100)
+temps = timeit.timeit('calculer_moindre_rectangle_python(inf,sup,n)',globals=globals(), number=100)
 print(f"Temps moyen par execution : {temps/1000*100:.6f} ms")
 
+### Methode numpy ###
 
+def calculer_moindre_rectangle_numpy(a,b,c,d,inf,sup,n):
+    # Créer les points x entre inf et sup
+    largeur_segment = (sup - inf)/n
+    valeur_x = np.linspace(inf, sup, n+1)
+    valeur_y = np.polyval([d, c, b, a], valeur_x) 
+    hauteur_segment = (valeur_y[:-1] + valeur_y[1:]) / 2 # Calculer la hauteur moyenne de chaque rectangle
+    # Calculer l'aire totale
+    aire_totale = np.sum(hauteur_segment * largeur_segment)
+    
+    return aire_totale
+
+print("Methode numerique numpy : ",calculer_moindre_rectangle_numpy(a,b,c,d,inf,sup,n))
+
+temps = timeit.timeit('calculer_moindre_rectangle_numpy(a,b,c,d,inf,sup,n)',globals=globals(), number=100)
+print(f"Temps moyen par execution pour numpy: {temps/1000*100:.6f} ms")
