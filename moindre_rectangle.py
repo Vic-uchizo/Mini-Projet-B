2da9f2102969a6438fd8e85a41480272dd77f885
kille <killegavre@gmail.com>
2026-06-08 09:08:30 -0400

    retirer erreur_python_numerique et le test autonome de mondres_rectangles.py

--- moindre_rectangle.py
+++ moindre_rectangle.py
@@ -16,12 +16,6 @@ def calculer_moindre_rectangle_python(inf, sup, f, n):
     return Aire_rectangle_tot
 
 
-def erreur_python_numerique(inf, sup, f, n,):
-    """Ecart entre la valeur numerique (rectangles) et la valeur exacte."""
-    numerique = calculer_moindre_rectangle_python(inf, sup, f, n)
-    exacte = calculer_methode_analytique(f, inf, sup)
-    return abs(numerique - exacte)
-
 def calculer_moindre_rectangle_numpy(inf,sup,f,n):
     # Créer les points x entre inf et sup
     largeur_segment = (sup - inf)/n
@@ -32,19 +26,3 @@ def calculer_moindre_rectangle_numpy(inf,sup,f,n):
     aire_totale = np.sum(hauteur_segment * largeur_segment)
     
     return aire_totale
-
-
-# Test autonome (ne s'execute QUE si on lance ce fichier directement).
-if __name__ == "__main__":
-    a, b, c, d = 3, 2, 5, 1
-    inf, sup, n = 40, 50, 10
-
-    def polynome(x):
-        return a + b*x + c*x**2 + d*x**3
-
-    polynome.coeff=[a,b,c,d]
-
-    print("Analytique :", calculer_methode_analytique(polynome, inf, sup))
-    print("Numerique  :", calculer_moindre_rectangle_python(inf, sup, polynome, n))
-    print("Erreur     :", erreur_python_numerique(inf, sup, polynome, n ))
-    print("Methode moindre carre : ",calculer_moindre_rectangle_numpy(inf,sup,polynome,n))
